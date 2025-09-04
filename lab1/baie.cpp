f05798259778429fef66ecabb7346dee79491014
Tai Do <donhutai@gmail.com>
2025-09-04 03:02:42 +0000

    lab1

---
+++ lab1/baie.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int findMax(int num1, int num2, int num3) {
+    int max = 0;
+    if ((num1 > num2) && (num1 > num3))
+        max = num1;
+
+    if ((num2 > num1) && (num2 > num3))
+        max = num2;
+
+    if ((num3 > num1) && (num3 > num2))
+        max = num3;
+    
+    return max;
+}
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << findMax(a, b, c) << endl;
+    return 0;
+}
